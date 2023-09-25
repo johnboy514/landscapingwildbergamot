1363cc6794627ed4c334b053aae278c3c64efd4b
John Miller <john.ifert.miller@gmail.com>
2023-09-25 16:14:10 -0400

    changed some footer stuff

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,45 +6,11 @@ function Footer() {
               <div className="row mt-3">             
                 <div className="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">              
                   <h6 className="text-uppercase fw-bold mb-4">
-                    <i className="fas fa-gem me-3 text-secondary"></i>Company name
+                    <i className="fas fa-gem me-3 text-secondary"></i>Wild Bergamot
                   </h6>
                   <p>
                     Here you can use rows and columns to organize your footer content. Lorem ipsum
                     dolor sit amet, consectetur adipisicing elit.
-                  </p>
-                </div>         
-                <div className="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">        
-                  <h6 className="text-uppercase fw-bold mb-4">
-                    Products
-                  </h6>
-                  <p>
-                    <a href="#!" className="text-reset">Angular</a>
-                  </p>
-                  <p>
-                    <a href="#!" className="text-reset">React</a>
-                  </p>
-                  <p>
-                    <a href="#!" className="text-reset">Vue</a>
-                  </p>
-                  <p>
-                    <a href="#!" className="text-reset">Laravel</a>
-                  </p>
-                </div>             
-                <div className="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">            
-                  <h6 className="text-uppercase fw-bold mb-4">
-                    Useful links
-                  </h6>
-                  <p>
-                    <a href="#!" className="text-reset">Pricing</a>
-                  </p>
-                  <p>
-                    <a href="#!" className="text-reset">Settings</a>
-                  </p>
-                  <p>
-                    <a href="#!" className="text-reset">Orders</a>
-                  </p>
-                  <p>
-                    <a href="#!" className="text-reset">Help</a>
                   </p>
                 </div>
                 <div className="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
