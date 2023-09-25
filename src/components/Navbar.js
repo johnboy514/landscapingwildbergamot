ddce0752ac1e48d365bbbf82541cf90e28790923
rjo6615 <robertjobernier@gmail.com>
2023-09-25 16:14:17 -0400

    navbar styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,19 @@
 import Container from 'react-bootstrap/Container';
-import Navbar from 'react-bootstrap/Navbar';
+import { Navbar, Nav } from 'react-bootstrap';
+
 
 function NavbarComponent() {
   return (
 <Navbar expand="lg" className="shadow-lg mb-5">
     <Container fluid>
     <img src="../images/wildbergamotlogo.png" className="img-fluid" alt="logo" style={{width: '50px', height: 'auto', marginLeft: 'auto'}}></img>
-      <Navbar.Brand style={{  marginRight: 'auto' }}>Wild Bergamot</Navbar.Brand>
+      <Navbar.Brand style={{  marginRight: '5px' }}>Wild Bergamot</Navbar.Brand>
+      <br></br>
+          <Nav style={{  marginRight: 'auto', marginLeft: '5px' }}>
+            <Nav.Link href="#home">About</Nav.Link>
+            <Nav.Link href="#features">Services</Nav.Link>
+            <Nav.Link href="#pricing">Request a Quote</Nav.Link>
+          </Nav>
     </Container>
 </Navbar>
   );
