9386c42f69908ae55f19b51172ca295640a5b9b3
rjo6615 <robertjobernier@gmail.com>
2023-09-28 15:20:15 -0400

    changed styling and colors of navbar and home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,51 +1,128 @@
-import Carousel from 'react-bootstrap/Carousel';
-
-
+import React from "react";
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import { useRef } from "react";
+import emailjs from '@emailjs/browser';
 
 function Home() {
-    return (
-      <center>
-        <div style={{maxWidth: "1000px"}}>
-        <Carousel>
-      <Carousel.Item>
-      <img
-          className="d-block w-100"
-          src="https://thumbor.forbes.com/thumbor/fit-in/900x510/https://www.forbes.com/home-improvement/wp-content/uploads/2022/07/featured-image-cheap-landscaping.jpeg"
-          alt="First slide"
-        />
-        <Carousel.Caption>
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-      <img
-          className="d-block w-100"
-          src="https://thumbor.forbes.com/thumbor/fit-in/900x510/https://www.forbes.com/home-improvement/wp-content/uploads/2022/07/featured-image-front-yard-landscaping.jpeg"
-          alt="Second slide"
-        />
-        <Carousel.Caption>
-          <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-      <img
-          className="d-block w-100"
-          src="https://thumbor.forbes.com/thumbor/fit-in/900x510/https://www.forbes.com/home-improvement/wp-content/uploads/2022/04/featured-image-landscaping.jpeg-1.jpg"
-          alt="Third slide"
-        />
-        <Carousel.Caption>
-          <h3>Third slide label</h3>
-          <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-          </p>
-        </Carousel.Caption>
-      </Carousel.Item>
-    </Carousel>
+
+  const [visible, setVisible] = React.useState(false);
+  const [visible1, setVisible1] = React.useState(false);
+  const [visible2, setVisible2] = React.useState(false);
+  const [visible3, setVisible3] = React.useState(false);
+  const name = useRef();
+  const email = useRef();
+  const message = useRef();
+  const address = useRef();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (name.current.value === '') {
+      setVisible(true);
+    } if (email.current.value === '') {
+      setVisible2(true);
+    } if (message.current.value === '') {
+      setVisible1(true);
+    } if (address.current.value === '') {
+      setVisible3(true);
+    } else {
+      sendMail();
+    }
+  };
+
+  (function () {
+    emailjs.init("lXIIIeccwD3sd6Wtl");
+  })();
+
+  function sendMail() {
+    if (name.current.value && email.current.value && email.current.value) {
+      var params = {
+        from_name: name.current.value,
+        from_email: email.current.value,
+        reply_to: email.current.value,
+        message: message.current.value,
+        address: address.current.value,
+      };
+      emailjs.send('service_0y00gzg', 'template_jff3baf', params).then(function (res) {});
+      alert("Thank you for sending a message!");
+      window.location.reload(false);
+    } else {
+      alert('Failed to send message');
+    }
+  };
+
+  const pageStyle = {
+    backgroundImage: 'url("https://i.pinimg.com/originals/db/f7/c7/dbf7c77d5f6b397743c0b431f4bfcb6a.jpg")',
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat',
+    backgroundPosition: 'center',
+    minHeight: '750px',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'column',
+    color: '#fefae0',
+  };
+
+  return (
+    <div style={pageStyle}>
+      <section className="">
+        <div className="px-4 py-5 px-md-5 text-center text-lg-start">
+          <div className="container">
+            <div className="row gx-lg-5 align-items-center">
+              <div className="col-lg-6 mb-5 mb-lg-0">
+                <h1 className="my-5 display-3 fw-bold ls-tight">
+                  The best offer <br />
+                  <span style={{ color: '#bc6c25' }}>for your business</span>
+                </h1>
+                <p style={{ color: "hsl(217, 10%, 50.8%)" }}>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Eveniet, itaque accusantium odio, soluta, corrupti aliquam
+                  quibusdam tempora at cupiditate quis eum maiores libero
+                  veritatis? Dicta facilis sint aliquid ipsum atque?
+                </p>
+              </div>
+
+              <div className="col-lg-6 mb-5 mb-lg-0">
+                <div className="card" style={{ maxWidth: '400px', maxHeight: '600px', backgroundColor: '#dda15e', border: '2px solid #283618' }}>
+                  <div className="card-body py-5 px-md-">
+                    <Form onSubmit={handleSubmit} className="px-3">
+                      <Form.Group className="mb-3 pt-3" controlId="formName">
+                        <Form.Control type="text" ref={name} placeholder="Name" />
+                        {visible && <div className="text-danger">Please enter your name</div>}
+                      </Form.Group>
+
+                      <Form.Group className="mb-3" controlId="formEmail">
+                        <Form.Control type="email" ref={email} placeholder={"Email"} />
+                        {visible2 && <div className="text-danger">Please enter your Email</div>}
+                      </Form.Group>
+
+                      <Form.Group className="mb-3" controlId="formAddress">
+                        <Form.Control type="text" ref={address} placeholder="Job site address" />
+                        {visible3 && <div className="text-danger">Please enter your job site address</div>}
+                      </Form.Group>
+
+                      <Form.Group className="mb-3" controlId="formMessage">
+                      <Form.Control as="textarea" ref={message} rows={5} placeholder="Enter your message..."/>
+                        {visible1 && <div className="text-danger">Please enter your message</div>}
+                      </Form.Group>
+                      <div style={{ borderBottom: '2px solid #283618' }}></div>
+                      
+                      <div className="d-flex justify-content-center mt-3"> {/* Center the button */}
+                        <Button style={{ backgroundColor: '#283618' }} type="submit">
+                          Submit
+                        </Button>
+                      </div>
+                    </Form>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-        </center>
-    );
+      </section>
+    </div>
+  );
 }
 
 export default Home;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,24 +1,61 @@
 import Container from 'react-bootstrap/Container';
 import { Navbar, Nav } from 'react-bootstrap';
 
-
 function NavbarComponent() {
+  const navbarStyle = {
+    backgroundColor: "#606c38",
+    position: 'relative',
+    height: '110px',
+  };
+
+  const lineStyle = {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    height: '25px',
+    backgroundColor: "#283618",
+  };
+
+  const whiteTextStyle = {
+    color: '#fefae0',
+    marginTop: '25px', // Add margin to push the text down
+  };
+
+  const logoStyle = {
+    width: '50px',
+    height: 'auto',
+    marginTop: '25px', // Add margin to push the image down
+  };
+
+  const lineAboveNavLinksStyle = {
+    position: 'absolute',
+    top: '100px', // Position the line below the Navbar
+    left: 0,
+    right: 0,
+    backgroundColor: "#283618",
+  };
+
   return (
-<Navbar expand="lg" className="shadow-lg mb-5">
-    <Container fluid>
-    <img src="../images/wildbergamotlogo.png" className="img-fluid" alt="logo" style={{width: '50px', height: 'auto' }}></img>
-      <Navbar.Brand style={{  marginRight: 'auto', marginLeft: 'auto' }}>Wild Bergamot</Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav>
-            <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="/about">About</Nav.Link>
-            <Nav.Link href="/services">Services</Nav.Link>
-            <Nav.Link href="/request-a-quote">Request a Quote</Nav.Link>
-          </Nav>
+    <div style={navbarStyle}>
+      <div style={lineStyle}></div>
+      <Navbar expand="lg">
+        <Container fluid>
+          <img src="../images/wildbergamotlogo.png" className="img-fluid" alt="logo" style={logoStyle}></img>
+          <Navbar.Brand style={{ ...whiteTextStyle }}>Wild Bergamot</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav>
+              <Nav.Link href="/" style={whiteTextStyle}>Home</Nav.Link>
+              <Nav.Link href="/about" style={whiteTextStyle}>About</Nav.Link>
+              <Nav.Link href="/services" style={whiteTextStyle}>Services</Nav.Link>
+              <Nav.Link href="/request-a-quote" style={whiteTextStyle}>Request a Quote</Nav.Link>
+            </Nav>
           </Navbar.Collapse>
-    </Container>
-</Navbar>
+        </Container>
+      </Navbar>
+      <div style={lineAboveNavLinksStyle}></div>
+    </div>
   );
 }
 
