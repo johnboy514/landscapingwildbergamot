0959e3f03b41f0fafdf0ff0cbae40ec9f39b3edf
rjo6615 <robertjobernier@gmail.com>
2023-11-06 13:53:06 -0500

    added logo and removed background of logo

--- src/components/Home.js
+++ src/components/Home.js
@@ -71,6 +71,7 @@ function Home() {
           <div className="container">
             <div className="row gx-lg-5 align-items-center">
               <div className="col-lg-6 mb-5 mb-lg-0 mt-5">
+              <img src="images\wildbergamotlogo.png" alt="Logo" className="img-fluid float-start" style={{ marginTop: '20px' }} />
                 <h1 className="my-5 display-3 fw-bold ls-tight">
                   Maintain <br />
                   <span style={{ color: '#fefae0' }}>Your Estate</span>
